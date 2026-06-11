5e8099c894ebdf8e89805f300347596dabbb401e
Octaver <Octaver2131@outlook.com>
2026-06-11 18:48:05 +0800

    feat: user Login

--- src/main/java/com/octaver/octnocode/controller/UserController.java
+++ src/main/java/com/octaver/octnocode/controller/UserController.java
@@ -4,9 +4,12 @@ import com.octaver.octnocode.common.BaseResponse;
 import com.octaver.octnocode.common.ResultUtils;
 import com.octaver.octnocode.exception.ErrorCode;
 import com.octaver.octnocode.exception.ThrowUtils;
+import com.octaver.octnocode.model.dto.UserLoginRequest;
 import com.octaver.octnocode.model.dto.UserRegisterRequest;
+import com.octaver.octnocode.model.vo.LoginUserVO;
 import com.octaver.octnocode.service.UserService;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,21 @@ public class UserController {
         String checkPassword = userRegisterRequest.getCheckPassword();
         long result = userService.userRegister(userAccount, userPassword, checkPassword);
         return ResultUtils.success(result);
+    }
+
+    /**
+     * 用户登录
+     *
+     * @param userLoginRequest 用户注册请求
+     * @param request 请求对象
+     * @return 脱敏后的用户登录信息
+     */
+    @PostMapping("/login")
+    public BaseResponse<LoginUserVO> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(userLoginRequest == null, ErrorCode.PARAMS_ERROR);
+        String userAccount = userLoginRequest.getUserAccount();
+        String userPassword = userLoginRequest.getUserPassword();
+        LoginUserVO loginUserVO = userService.userLogin(userAccount, userPassword, request);
+        return ResultUtils.success(loginUserVO);
     }
 }
--- src/main/java/com/octaver/octnocode/service/UserService.java
+++ src/main/java/com/octaver/octnocode/service/UserService.java
@@ -2,6 +2,8 @@ package com.octaver.octnocode.service;
 
 import com.mybatisflex.core.service.IService;
 import com.octaver.octnocode.model.entity.User;
+import com.octaver.octnocode.model.vo.LoginUserVO;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * 用户 服务层。
@@ -19,6 +21,23 @@ public interface UserService extends IService<User> {
      * @return 新用户 id
      */
     long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
 
     /**
      * 加密
