efa6453e21ebb23f6fbe374899c9c883a0a3cfd9
Octaver <Octaver2131@outlook.com>
2026-06-13 18:02:19 +0800

    feat: 代码生成门面模式

---
+++ src/test/java/com/octaver/octnocode/core/AiCodeGeneratorFacadeTest.java
@@ -0,0 +1,24 @@
+package com.octaver.octnocode.core;
+
+import com.octaver.octnocode.model.emus.CodeGenTypeEnum;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiCodeGeneratorFacadeTest {
+
+    @Resource
+    private AiCodeGeneratorFacade aiCodeGeneratorFacade;
+
+    @Test
+    void generateAndSaveCode() {
+        File file = aiCodeGeneratorFacade.generateAndSaveCode("生成电子简历", CodeGenTypeEnum.HTML);
+        Assertions.assertNotNull(file);
+    }
+}
