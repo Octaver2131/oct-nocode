1aa6074d48757224f0ad648039ee12584d47ed6a
Octaver <Octaver2131@outlook.com>
2026-06-13 19:13:04 +0800

    feat: SSE 流式输出

--- src/main/java/com/octaver/octnocode/core/AiCodeGeneratorFacade.java
+++ src/main/java/com/octaver/octnocode/core/AiCodeGeneratorFacade.java
@@ -7,7 +7,9 @@ import com.octaver.octnocode.exception.BusinessException;
 import com.octaver.octnocode.exception.ErrorCode;
 import com.octaver.octnocode.model.emus.CodeGenTypeEnum;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.io.File;
 
@@ -15,6 +17,7 @@ import java.io.File;
  * AI 代码生成外观类，组合生成和保存功能
  */
 @Service
+@Slf4j
 public class AiCodeGeneratorFacade {
 
     @Resource
@@ -42,6 +45,27 @@ public class AiCodeGeneratorFacade {
     }
 
     /**
+     * 统一入口：根据类型生成并保存代码（流式）
+     *
+     * @param userMessage     用户提示词
+     * @param codeGenTypeEnum 生成类型
+     * @return 保存的目录
+     */
+    public Flux<String> generateAndSaveCodeStream(String userMessage, CodeGenTypeEnum codeGenTypeEnum) {
+        if (codeGenTypeEnum == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "生成类型为空");
+        }
+        return switch (codeGenTypeEnum) {
+            case HTML -> generateAndSaveHtmlCodeStream(userMessage);
+            case MULTI_FILE -> generateAndSaveMultiFileCodeStream(userMessage);
+            default -> {
+                String errorMessage = "不支持的生成类型：" + codeGenTypeEnum.getValue();
+                throw new BusinessException(ErrorCode.SYSTEM_ERROR, errorMessage);
+            }
+        };
+    }
+
+    /**
      * 生成 HTML 模式的代码并保存
      *
      * @param userMessage 用户提示词
@@ -61,5 +85,61 @@ public class AiCodeGeneratorFacade {
     private File generateAndSaveMultiFileCode(String userMessage) {
         MultiFileCodeResult result = aiCodeGeneratorService.generateMultiFileCode(userMessage);
         return CodeFileSaver.saveMultiFileCodeResult(result);
+    }
+
+    /**
+     * 生成 HTML 模式的代码并保存（流式）
+     *
+     * @param userMessage 用户提示词
+     * @return 保存的目录
+     */
+    private Flux<String> generateAndSaveHtmlCodeStream(String userMessage) {
+        Flux<String> result = aiCodeGeneratorService.generateHtmlCodeStream(userMessage);
+        // 定义字符串拼接器，用于当流式返回所有的代码之后，再保存代码
+        StringBuilder codeBuilder = new StringBuilder();
+        return result.doOnNext(chunk -> {
+            // 实时收集代码片段
+            codeBuilder.append(chunk);
+        }).doOnComplete(() -> {
+            // 流式返回完成后，保存代码
+            try {
+                String completeHtmlCode = codeBuilder.toString();
+                // 解析代码为对象
+                HtmlCodeResult htmlCodeResult = CodeParser.parseHtmlCode(completeHtmlCode);
+                // 保存代码到文件
+                File saveDir = CodeFileSaver.saveHtmlCodeResult(htmlCodeResult);
+                log.info("代码保存成功，保存目录：{}", saveDir.getAbsolutePath());
+            } catch (Exception e) {
+                log.error("保存失败：{}", e.getMessage());
+            }
+        });
+    }
+
+    /**
+     * 生成多文件模式的代码并保存（流式）
+     *
+     * @param userMessage 用户提示词
+     * @return 保存的目录
+     */
+    private Flux<String> generateAndSaveMultiFileCodeStream(String userMessage) {
+        Flux<String> result = aiCodeGeneratorService.generateMultiFileCodeStream(userMessage);
+        // 定义字符串拼接器，用于当流式返回所有的代码之后，再保存代码
+        StringBuilder codeBuilder = new StringBuilder();
+        return result.doOnNext(chunk -> {
+            // 实时收集代码片段
+            codeBuilder.append(chunk);
+        }).doOnComplete(() -> {
+            // 流式返回完成后，保存代码
+            try {
+                String completeMultiFileCode = codeBuilder.toString();
+                // 解析代码为对象
+                MultiFileCodeResult multiFileCodeResult = CodeParser.parseMultiFileCode(completeMultiFileCode);
+                // 保存代码到文件
+                File saveDir = CodeFileSaver.saveMultiFileCodeResult(multiFileCodeResult);
+                log.info("代码保存成功，保存目录：{}", saveDir.getAbsolutePath());
+            } catch (Exception e) {
+                log.error("保存失败：{}", e.getMessage());
+            }
+        });
     }
 }
